0cc4f7c5725c8cf1a074619cb5faaec2deec8477
GuilleCalM <guilleacalderon@hotmail.com>
2024-10-23 18:15:31 -0500

    first commit

---
+++ src/main/java/com/cibertec/edu/api_externa/DswiiEfCalderonMontesGuillermoAnthoniApiExternaApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.edu.api_externa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiEfCalderonMontesGuillermoAnthoniApiExternaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DswiiEfCalderonMontesGuillermoAnthoniApiExternaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cibertec/edu/api_externa/DswiiEfCalderonMontesGuillermoAnthoniApiExternaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.edu.api_externa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiEfCalderonMontesGuillermoAnthoniApiExternaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
